dad11cbf1e6e8698c75fa265209b73a8ace90f4e
TREANGLE <73377812+fatih409@users.noreply.github.com>
2021-05-16 21:14:05 +0300

    bilgi toplama
    Site Bilgi Toplama Aracıdır

---
+++ bilgi.toplama.py
@@ -0,0 +1,23 @@
+import os
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet BİLGİ TOPLAMA")
+print("-"*45)
+print("Bu Araç Treangle Tarafından yapılmıştır. İnstagram = zumrudu_anka_team")
+print("-"*45)
+
+
+ftp = input("site Ipsi giriniz : ")
+w_list = input("Wordlist Giriniz : ")
+os.system("dmitry " + ftp + " -o " + w_list)
+
+
+
+
+
+
+
+
+
+
+
